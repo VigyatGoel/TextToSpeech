b0fb6bd9f34bedcea80023dfd6642f6150d235dd
VigyatGoel <vigyatgoel@gmail.com>
2024-04-02 15:53:14 +0530

    Made code more modular and added support for platform identification using python's platform module

--- Main.py
+++ Main.py
@@ -1,20 +1,35 @@
 import os
+import platform
 
-if __name__ == '__main__':
 
+def text_to_speech():
     print("Hello, Welcome to Text-to-Speech engine created by Vigyat Goel ")
 
-    print("If you are using mac then well and good, but if you are using windows then bye bye.")
+    if platform.system() == 'Windows':
+        print("Sorry, this program does not support Windows.")
+        return
+    elif platform.system() != 'Darwin':  # macOS
+        print("Sorry, this program only supports macOS.")
+        return
 
     while True:
-
         x = input("Enter what you want me to speak or press q to exit : ")
 
         if x == "q":
-
             print("Bye bye friend.")
             os.system("say 'Bye bye friend'")
             break
 
-        command = f"say {x}"
-        os.system(command)
+        try:
+            command = f"say {x}"
+            os.system(command)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+def main():
+    text_to_speech()
+
+
+if __name__ == '__main__':
+    main()
